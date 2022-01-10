fad05f6a2c7fcea06f3e094ed8b7a83f1bdb38e8
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-10 16:47:02 +0600

    initialised firebase and added firestore rules, storage rules & hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQTPN7Cnc9nH4hK3fiEf4BMiAeGFkky4o",
+  authDomain: "proligz.firebaseapp.com",
+  projectId: "proligz",
+  storageBucket: "proligz.appspot.com",
+  messagingSenderId: "883005056057",
+  appId: "1:883005056057:web:0cce7b4b1fc49e337999e8",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectAuth, projectFirestore, timestamp };
