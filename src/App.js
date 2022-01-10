0d348a87afdbe0d0efc3e9a95a12e7bff3a9baa7
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-10 20:05:46 +0600

    added browserRouter and different routes to the project.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//components
+
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+
+//styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>working!</p>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
