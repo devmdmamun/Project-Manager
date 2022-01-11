4bb00c9e69ee80d194d679662355c9054fc300aa
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-11 20:15:19 +0600

    added a basic signup design

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,76 @@
+import { useState } from "react";
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError("please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("selected file must be image");
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError("image file must be less then 100KB");
+      return;
+    }
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log("thmbnail updated");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Signup</h2>
+      <label>
+        <span>Name: </span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email: </span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Profile Image: </span>
+        <input required type="file" onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
+      </label>
+      <label>
+        <span>Password: </span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
