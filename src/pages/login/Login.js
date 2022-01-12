453cf52175dd3701f38ce48486a012e0e8d7570c
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-12 14:25:55 +0600

    created a basic template

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
 import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Login</h2>
+      <label>
+        <span>Email: </span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password: </span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Log in</button>
+      {/* {!isPending && <button className="btn">Log in</button>} */}
+      {/* {isPending && (
+        <button disabled className="btn">
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>} */}
+    </form>
+  );
 }
