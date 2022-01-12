e69b8f8ae546449e13d92e4df882441675edf217
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-12 19:49:42 +0600

    conditional rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 import "./Navbar.css";
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className="navbar">
@@ -11,25 +13,32 @@ export default function Navbar() {
         <li className="logo">
           <h2>Proligz</h2>
         </li>
-
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button onClick={logout} className="btn">
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button disabled className="btn">
-              Logging out
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              {!isPending && (
+                <button onClick={logout} className="btn">
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button disabled className="btn">
+                  Logging out
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
