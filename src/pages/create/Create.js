ad734d366b9ff0c1c8a8efd387aafa7fe70460e4
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-14 21:55:12 +0600

    custom error if !selected any option

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -20,6 +20,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState("");
   const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
 
   useEffect(() => {
     if (document) {
@@ -32,6 +33,17 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError(null);
+
+    if (!category) {
+      setFormError("Please select a project category");
+      return;
+    }
+    if (assignedUsers.length < 1) {
+      setFormError("Please assign the project to at least 1 user");
+      return;
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers);
   };
 
@@ -83,6 +95,7 @@ export default function Create() {
           />
         </label>
         <button className="btn">Add project</button>
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
