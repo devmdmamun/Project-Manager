fcc96c6903effd3a38ad2e955695799486a16b1e
Mamun <md.mamun.islam.santo@gmail.com>
2022-01-18 13:26:23 +0600

    bare minimum to fix overflow issue

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -85,55 +85,57 @@ export default function Create() {
   };
 
   return (
-    <div className="create-form">
-      <h2 className="page-title"> Create a new project </h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Project name</span>
-          <input
-            required
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-        </label>
-        <label>
-          <span>Project details</span>
-          <textarea
-            required
-            type="text"
-            onChange={(e) => setDetails(e.target.value)}
-            value={details}
-          ></textarea>
-        </label>
-        <label>
-          <span>Due date</span>
-          <input
-            required
-            type="date"
-            onChange={(e) => setDueDate(e.target.value)}
-            value={dueDate}
-          />
-        </label>
+    <div className="create-container">
+      <div className="create-form">
+        <h2 className="page-title"> Create a new project </h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Project name</span>
+            <input
+              required
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </label>
+          <label>
+            <span>Project details</span>
+            <textarea
+              required
+              type="text"
+              onChange={(e) => setDetails(e.target.value)}
+              value={details}
+            ></textarea>
+          </label>
+          <label>
+            <span>Due date</span>
+            <input
+              required
+              type="date"
+              onChange={(e) => setDueDate(e.target.value)}
+              value={dueDate}
+            />
+          </label>
 
-        <label>
-          <span>Project category:</span>
-          <Select
-            onChange={(option) => setCategory(option)}
-            options={categories}
-          />
-        </label>
-        <label>
-          <span>Assign to:</span>
-          <Select
-            onChange={(option) => setAssignedUsers(option)}
-            options={users}
-            isMulti
-          />
-        </label>
-        <button className="btn">Add project</button>
-        {formError && <p className="error">{formError}</p>}
-      </form>
+          <label>
+            <span>Project category:</span>
+            <Select
+              onChange={(option) => setCategory(option)}
+              options={categories}
+            />
+          </label>
+          <label>
+            <span>Assign to:</span>
+            <Select
+              onChange={(option) => setAssignedUsers(option)}
+              options={users}
+              isMulti
+            />
+          </label>
+          <button className="btn">Add project</button>
+          {formError && <p className="error">{formError}</p>}
+        </form>
+      </div>
     </div>
   );
 }
